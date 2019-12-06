cac4addb6e245d9e3160cb850af5a99670257492
Josh <2107767@talnet.nl>
2019-12-06 11:16:53 +0100

    Uitwerking van de exercise

---
+++ wisselgeld.php
@@ -0,0 +1,58 @@
+<?php
+
+$input = ($argv[1]);
+
+is_numeric ($argv);
+
+try {
+    if ($input>0) {
+    } else {
+        throw new Exception ("Ik kan geen negatief bedrag wisselen");
+    } 
+}   catch (Exception $ax) {
+        echo "Error: ".$ax->getMessage() . PHP_EOL;
+    }
+
+ try {
+     if ($input>0) {
+    } else {
+        throw new Exception ("Je hebt geen bedrag meegegeven dat omgewisseld dient te worden");
+    }
+}   catch (Exception $ex) {
+        echo "Error: ".$ex->getMessage() . PHP_EOL;
+    }
+
+    try {
+        if ($input>0) {
+       } else {
+           throw new Exception ("Je hebt geen bedrag meegegeven");
+       }
+   }   catch (Exception $lx) {
+           echo "Error: ".$lx->getMessage() . PHP_EOL;
+       }
+
+$euroArray = array(50, 20, 10, 5, 2, 1);
+$centenArray = array(50, 20, 10, 5);
+
+
+$geld = intval($input);
+$cent = $input - $geld;
+$cent = intval(round($cent * 100));
+$cent = round($cent / 5) * 5;
+
+
+foreach ($euroArray as $value) {
+    $restgeld = floor($geld / $value);
+    if ($restgeld >= 1) {
+        $geld = $geld - ($value * $restgeld);
+        echo $restgeld . " x " . $value . " euro" . PHP_EOL;
+    }
+}
+
+foreach ($centenArray as $value) {
+    $restcent = floor($cent / $value);
+    if ($restcent >= 1) {
+        $cent = $cent - ($value * $restcent);
+        echo $restcent . " x " . $value . " cent" . PHP_EOL;
+    }
+}
